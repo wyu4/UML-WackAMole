35b4422fd2ab227eb63292888a7f5790d2c4309c
Wilson <wilsonyu657@gmail.com>
2024-04-25 01:25:15 -0400

    Removed extra game (test) in main method

--- src/GameTest.java
+++ src/GameTest.java
@@ -2,15 +2,15 @@ import java.awt.Rectangle;
 
 public class GameTest {
     public static WackAMole game1 = new WackAMole(new Rectangle(500, 600)); // Create a new game
-    public static WackAMole game2 = new WackAMole(new Rectangle( 500, 0, 1000,700)); // Create a new game
+    // public static WackAMole game2 = new WackAMole(new Rectangle( 500, 0, 1000,700)); // Create a new game
 
     public static void main(String[] args) throws Exception {
         game1.startGame(); // Start the game
 
-        try {
-            Thread.sleep(5000);
-        } catch(InterruptedException ignore) {}
+        // try {
+        //     Thread.sleep(5000);
+        // } catch(InterruptedException ignore) {}
 
-        game2.startGame();
+        // game2.startGame();
     }
 }
