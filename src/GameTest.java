dad5baa3599baf29b19e9fed3acad9ae22be3cc1
Wilson <wilsonyu657@gmail.com>
2024-04-25 03:57:24 -0400

    Added inputable dimensions to tester method

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,11 +1,39 @@
+import javax.swing.*;
 import java.awt.Rectangle;
 
 public class GameTest {
-    public static WackAMole game1 = new WackAMole(new Rectangle(500, 600)); // Create a new game
-
     // main method for testing purposes
     public static void main(String[] args) throws Exception {
+        // Getting the dimensions for the window
+        int width = getIntInput("What should the width of the field be?");
+        int height = getIntInput("What should the height of the field be?");
+
+        // Creating a new game using those dimensions
+        WackAMole game1 = new WackAMole(new Rectangle(width, height));
         game1.startGame(); // Start the game
+
         // View the different objects interacting inside the WackAMole class.
+    }
+
+    /**
+     * Get a user input and parse as integer
+     * @param message The question to ask to the user
+     * @return Parsed input
+     */
+    public static int getIntInput(String message) {
+        String input;
+        int parsedInput;
+
+        while (true) { // Loop through until told to break
+            input = JOptionPane.showInputDialog(message); // Get user input
+            try {
+                parsedInput = Integer.parseInt(input); // Try to parse as int
+                break; // Break out of loop if possible
+            } catch(NumberFormatException e) {
+                e.printStackTrace(); // Print error if the user input is not numerical
+            }
+        }
+
+        return parsedInput; // Return the parsed input
     }
 }
